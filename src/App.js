73df12939c56d6b2e083684e536c4c16d7e50fc8
Mike Jackson-Poter <nzmikejp@gmail.com>
2020-07-24 16:24:02 +1200

    HTML and CSS added to react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends Component {
+
+  constructor(props){
+    super(props)
+  }
+
+  render(){
+
+    return (
+      <div className="wrap">
+        <div className="container">
+            <header>
+                <h1>Daily todo list</h1>
+            </header>
+            <main>
+                <section className="section-1">
+                    <div className="input-group">
+                        <input type="text" id="todo" name="todo" placeholder="Add your todo"/>
+                        <i className="fas fa-plus plus"></i>
+                    </div>
+                    <div className="tasks">
+                        <div className="task">
+                            <div className="check-group">
+                                <input type="checkbox" id="task-1" name="task-1"/>
+                                <label htmlFor="task-1">Watch the godfather 1</label>
+                            </div>
+                            <div className="check-group-btns">
+                                <i className="fas fa-edit edit"></i>
+                                <i className="fas fa-trash-alt trash"></i>
+                            </div>
+                        </div>
+                        <div className="task">
+                            <div className="check-group">
+                                <input type="checkbox" id="task-1" name="task-1"/>
+                                <label htmlFor="task-1">Watch the godfather 1</label>
+                            </div>
+                            <div className="check-group-btns">
+                                <i className="fas fa-edit edit"></i>
+                                <i className="fas fa-trash-alt trash"></i>
+                            </div>
+                        </div>
+                        <div className="task">
+                            <div className="check-group">
+                                <input type="checkbox" id="task-1" name="task-1"/>
+                                <label htmlFor="task-1">Watch the godfather 1</label>
+                            </div>
+                            <div className="check-group-btns">
+                                <i className="fas fa-edit edit"></i>
+                                <i className="fas fa-trash-alt trash"></i>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </main>
+        </div>
     </div>
-  );
+    )
+
+  }
+
 }
 
 export default App;
