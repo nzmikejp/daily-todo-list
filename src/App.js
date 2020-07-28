3e631ad66a9fb3582ebe8ae54b4220ea9dd18427
Mike Jackson-Potter <nzmikejp@gmail.com>
2020-07-29 10:44:07 +1200

    checklist 2 states and events

--- src/App.js
+++ src/App.js
@@ -6,38 +6,55 @@ class App extends Component {
 
   constructor(props){
     super(props)
-    
-    this.tasks = [{
-        id: 1,
-        text: 'Watch the godfather 1',
-        date: 'Tue 6th March'
-    },{
-        id: 2,
-        text: 'Watch the godfather 2',
-        date: 'Wed 20th March'
-    },{
-        id: 3,
-        text: 'Watch the godfather 3',
-        date: 'Sun 1st April'
-    },{
-        id: 4,
-        text: 'Watch the godfather 4',
-        date: 'Sat 13th May'
-    },{
-        id: 5,
-        text: 'You have watched to many Movies. go for a walk',
-        date: 'Sun 14th May' 
-    },{
-        id: 6,
-        text: 'Stop slacking. Do some more React',
-        date: 'Mon 15th May' 
-    },{
-        id: 7,
-        text: 'Lean about Key id\'s in React to solve JS error',
-        date: 'Mon 15th May' 
-    }]
+
+    this.state = {
+
+        tasks: [{
+            id: 1,
+            text: 'Watch the godfather 1',
+            date: 'Tue 6th March'
+        }],
+
+        taskInputValue: '',
+        taskInputDate: ''
+    }
   }
 
+  handleInputChange = (e) => {
+      this.setState({taskInputValue: e.target.value})
+  }
+  
+  handleInputDateChange = (e) => {
+      this.setState({taskInputDate: e.target.value})
+  }
+
+  handleAddButton = (e) => {
+      e.preventDefault()
+      var task = {
+          id: Date.now(),
+          text: this.state.taskInputValue,
+          date: this.state.taskInputDate
+      }
+
+      var newTasks = [task, ...this.state.tasks]
+
+      this.setState({
+          tasks: newTasks,
+          taskInputValue: '',
+          taskInputDate: ''
+      })
+  }
+
+  handleTaskDelete = (e) => {
+      var taskIdToDelete = parseInt(e.target.id)
+      var tasks = this.state.tasks
+      var filterTasks = tasks.filter((item) => {
+          return item.id != taskIdToDelete
+      })
+
+      this.setState({tasks: filterTasks})
+
+  }
 
   render(){
 
@@ -49,18 +66,21 @@ class App extends Component {
             </header>
             <main>
                 <section className="section-1">
-                    <div className="input-group">
-                        <input type="text" id="todo" name="todo" placeholder="Add your todo"/>
-                        <i className="fas fa-plus plus"></i>
-                    </div>
+                    <form className="input-body">
+                        <div className="input-group">
+                            <input type="text" id="todo" name="todo" placeholder="Add your todo" value={this.state.taskInputValue} onChange={this.handleInputChange}/>
+                            <input type="text" id="date" name="date" placeholder="Add your due date" value={this.state.taskInputDate} onChange={this.handleInputDateChange}/>
+                        </div>
+                        <button type="submit" className="btn" onClick={this.handleAddButton}><i className="fas fa-plus plus"></i></button>
+                    </form>
                     <div className="tasks">
                         {
-                            this.tasks.map((task) =>{
+                            this.state.tasks.map((task) =>{
                                 return (
                                     <div className="task" key={task.id}>
                                         <div className="check-group">
-                                            <input type="checkbox" id="task" name="task"/>
-                                            <label htmlFor="task">
+                                            <input type="checkbox" id={task.id} name="task"/>
+                                            <label htmlFor={task.id}>
                                                 {task.text}
                                             </label>
                                             <div className="task-date">
@@ -69,7 +89,7 @@ class App extends Component {
                                         </div>
                                         <div className="check-group-btns">
                                             <i className="fas fa-edit edit"></i>
-                                            <i className="fas fa-trash-alt trash"></i>
+                                            <i id={task.id} className="fas fa-trash-alt trash" onClick={this.handleTaskDelete}></i>
                                         </div>
                                     </div>
                                 )
